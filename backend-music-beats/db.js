ab39f9c6f5e8316cfc693a5cc7ee4ddf8518287b
Elana10 <135548464+Elana10@users.noreply.github.com>
2024-05-29 14:02:42 -0500

    Update db.js
    Used for render db

--- backend-music-beats/db.js
+++ backend-music-beats/db.js
@@ -7,15 +7,16 @@ let db;
 
 if (process.env.NODE_ENV === "production") {
     db = new Client({
-      host : '/var/run/postgresql',
-      database: getDatabaseUri(),
+        connectionString: getDatabaseUri(),
+      // host : '/var/run/postgresql',
+      // database: getDatabaseUri(),
       ssl: {
         rejectUnauthorized: false
       }
     });
   } else {
     db = new Client({
-      host : '/var/run/postgresql',
+      // host : '/var/run/postgresql',
       database: getDatabaseUri()
     });
   }
